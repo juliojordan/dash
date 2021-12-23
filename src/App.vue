1e2c5096fc6a9ed9b0efad9fa86da4d6473d1b3a
Julio Jordán <juliojordan@protonmail.com>
2021-12-23 12:46:34 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table class="charts-css bar show-labels">
+      <tbody>
+        <tr v-for="[key, value] in arr" :key="key">
+          <th>{{ key }}</th>
+          <td :style="`--size:${value / 10};`"></td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="toggle">{{ msg }}</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return { data: {}, timer: null };
+  },
+  created() {
+    this.load();
+  },
+  destroyed() {
+    clearInterval(this.timer);
+  },
+  computed: {
+    arr() {
+      return Object.entries(this.data);
+    },
+    msg() {
+      return this.timer ? "Stop real time" : "Start real time";
+    },
+  },
+  methods: {
+    load() {
+      axios
+        .get("https://data.juliojordan.workers.dev/")
+        .then(({ data }) => {
+          this.data = data;
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    toggle() {
+      if (this.timer) {
+        clearInterval(this.timer);
+        this.timer = null;
+      } else {
+        this.timer = setInterval(this.load, 1000);
+      }
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "SF Mono", Monaco, monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table.bar {
+  height: 100px;
+  max-width: 300px;
+  margin: 0 auto;
 }
 </style>
